eaf4b8f88fac48a6f5f38deded685c30c36bc546
MG <emg110@gmail.com>
2022-09-08 20:27:12 +0000

    removed test.js

--- test.js
+++
@@ -1,4 +0,0 @@
-const algosdk = require('algosdk');
-console.log(algosdk.decodeUint64(Buffer.from(`AAAAAAAAAJY=`, "base64")))
-console.log(algosdk.decodeUint64(Buffer.from(`AAAAAAAAAAA=`, "base64")))
-console.log(algosdk.decodeUint64(Buffer.from(`AAAAAAAAAAs=`, "base64")))
